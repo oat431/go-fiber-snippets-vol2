b60d187d44a90a6878c5b0f5b998e73c4be3df2b
oat431 <oat431@gmail.com>
2026-03-10 17:48:29 +0700

    add: smtp example

--- internal/bootstrap/api_container.go
+++ internal/bootstrap/api_container.go
@@ -1,11 +1,23 @@
 package bootstrap
 
-import "github.com/gofiber/fiber/v3/log"
+import (
+	"oat431/go-fiber-snippets-vol2/internal/config"
+	"oat431/go-fiber-snippets-vol2/internal/controller"
+	"oat431/go-fiber-snippets-vol2/internal/service"
+
+	"github.com/gofiber/fiber/v3/log"
+)
 
 type AppContainer struct {
+	EmailController *controller.EmailController
 }
 
 func NewAppContainer() *AppContainer {
+	emailService := service.NewSMTPService(config.GetConfig())
+	emailController := controller.NewEmailController(*emailService)
+
 	log.Info("Starting Bootstrap Container...")
-	return &AppContainer{}
+	return &AppContainer{
+		EmailController: emailController,
+	}
 }
